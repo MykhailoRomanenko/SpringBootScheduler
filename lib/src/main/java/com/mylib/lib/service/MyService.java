ae850491ad53e5116ecf6796a02293e0b8777f8a
Artem Budaiev <abudaiev@olsom.net>
2021-10-20 19:22:49 +0300

    starter added 2.0

---
+++ lib/src/main/java/com/mylib/lib/service/MyService.java
@@ -0,0 +1,5 @@
+package com.mylib.lib.service;
+
+public interface MyService {
+    void myMethod();
+}
---
+++ lib/src/main/java/com/mylib/lib/service/MyServiceImpl.java
@@ -0,0 +1,8 @@
+package com.mylib.lib.service;
+
+public class MyServiceImpl implements MyService {
+    @Override
+    public void myMethod() {
+        System.out.println("Method from our custom starter invoked successfully!");
+    }
+}
