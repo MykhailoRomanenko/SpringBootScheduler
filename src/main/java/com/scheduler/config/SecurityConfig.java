f2bbc9c90a72a00f66afff3ce02c14f651afca39
RybakVolodymyr <volodymyr.y.rybak@gmail.com>
2021-11-24 20:59:11 +0200

    Thymeleaf auth changed

--- src/main/java/com/scheduler/config/SecurityConfig.java
+++ src/main/java/com/scheduler/config/SecurityConfig.java
@@ -1,28 +1,12 @@
 package com.scheduler.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtValidators;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Value("${auth0.audience}")
-    private String audience;
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuer;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -34,33 +18,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers(HttpMethod.PUT, "/api/v1/**").hasAuthority("write:all")
                 .mvcMatchers(HttpMethod.DELETE, "/api/v1/**").hasAuthority("write:all")
                 .anyRequest().permitAll()
-                .and().cors()
-                .and().oauth2ResourceServer().jwt()
-                .decoder(jwtDecoder())
-                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+                .and().oauth2Login();
     }
-
-    JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
-                JwtDecoders.fromOidcIssuerLocation(issuer);
-
-        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
-        jwtDecoder.setJwtValidator(withAudience);
-
-        return jwtDecoder;
-    }
-
-    JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
-        converter.setAuthoritiesClaimName("permissions");
-        converter.setAuthorityPrefix("");
-
-        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
-        jwtConverter.setJwtGrantedAuthoritiesConverter(converter);
-        return jwtConverter;
-    }
-
 }
--- src/main/java/com/scheduler/controller/HomeController.java
+++ src/main/java/com/scheduler/controller/HomeController.java
@@ -1,28 +1,21 @@
 package com.scheduler.controller;
 
-import com.scheduler.client.Auth0Client;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping(path = "/")
 public class HomeController {
 
-    private final Auth0Client auth0Client;
-
-    public HomeController(Auth0Client auth0Client) {
-        this.auth0Client = auth0Client;
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
     }
-
-//    @GetMapping(value = "/")
-//    public RedirectView home() {
-//        return new RedirectView(auth0Client.formAuthorizationUrl());
-//    }
 
     @GetMapping(value = "/test")
     public String test(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
