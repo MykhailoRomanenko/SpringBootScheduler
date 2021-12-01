9466bd8f83b8c1a600cedf9dcbc4320be8afdf2a
Artem Budaiev <abudaiev@olsom.net>
2021-12-01 22:40:16 +0200

    exec time logging and method args in/out logging added

---
+++ src/main/java/com/scheduler/aspects/MethodExecutingTimeAspect.java
@@ -0,0 +1,18 @@
+package com.scheduler.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class MethodExecutingTimeAspect {
+    @Around("@annotation(com.scheduler.aspects.TrackTime)")
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+        log.info("Time Taken by {} is {}", joinPoint, timeTaken);
+    }
+}
